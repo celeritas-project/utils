8c0f5fefe271243770cca89090d48cc690dede65
Stefano Tognini <togninis@ornl.gov>
2025-09-11 09:39:33 -0400

    Remove unnecessary `Detector` from detector construction class names

--- gdml-generator/src/ThinSlab.hh
+++ gdml-generator/src/ThinSlab.hh
@@ -1,9 +1,9 @@
 //----------------------------------*-C++-*----------------------------------//
-// Copyright 2022-2023 UT-Battelle, LLC, and other Celeritas developers.
+// Copyright 2024-2025 UT-Battelle, LLC, and other Celeritas developers.
 // See the top-level COPYRIGHT file for details.
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
-//! \file BoxDetector.hh
+//! \file ThinSlab.hh
 //! \brief Create the detector geometry.
 //---------------------------------------------------------------------------//
 #pragma once
@@ -17,21 +17,26 @@
 /*!
  * Construct a programmatic detector geometry.
  */
-class BoxDetector : public G4VUserDetectorConstruction
+class ThinSlab : public G4VUserDetectorConstruction
 {
   public:
     // Construct
-    BoxDetector();
+    ThinSlab();
 
     // Construct geometry
     G4VPhysicalVolume* Construct() final;
-
     // Set up sensitive detectors and magnetic field
     void ConstructSDandField() final;
 
   private:
-    // "Infinite" box: Pb cube with 500 m side
-    G4VPhysicalVolume* create_box();
+    struct SlabDefinition
+    {
+        G4Material* material;
+        std::array<double, 3> dimension;
+    };
+
+    // Thin slab for MSC validation
+    G4VPhysicalVolume* create_slab(SlabDefinition const& def);
     // Set volume as sensitive detector
     void set_sd();
 };
