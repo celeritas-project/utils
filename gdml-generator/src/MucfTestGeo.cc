9f7ca6d6a193f276d1db82f78a981f851c59fa37
Stefano Tognini <togninis@ornl.gov>
2025-10-21 14:54:05 -0400

    FIx copy numbers; Fix detector size bug

--- gdml-generator/src/MucfTestGeo.cc
+++ gdml-generator/src/MucfTestGeo.cc
@@ -45,8 +45,6 @@ G4VPhysicalVolume* MucfTestGeo::Construct()
 void MucfTestGeo::ConstructSDandField()
 {
     this->set_sd();
-
-    // TODO: Add magnetic field
 }
 
 //---------------------------------------------------------------------------//
@@ -57,7 +55,7 @@ void MucfTestGeo::ConstructSDandField()
 /*!
  * MuCF test geometry.
  *
- * Comprises a muon collimator, a d-t mixture target and neutron counters.
+ * Comprises a d-t mixture target and neutron counters.
  * Based on https://github.com/nklabs/muonic-atom-processes .
  */
 G4VPhysicalVolume* MucfTestGeo::build_geometry()
@@ -69,11 +67,13 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
 
     G4NistManager* nist = G4NistManager::Instance();
 
+    double const g_per_mole = CLHEP::g / CLHEP::mole;
+
     // Isotopes and elemental definition
     auto* hydrogen_iso = new G4Isotope("P",
                                        NumIsotopeComponents{1},
                                        NumberOfNucleons{1},
-                                       MoleMass{1.00794 * g / mole});
+                                       MoleMass{1.00794 * g_per_mole});
     auto* hydrogen_elem
         = new G4Element("Protium", "P", NumIsotopeComponents{1});
     hydrogen_elem->AddIsotope(hydrogen_iso, /* abundance fraction */ 1);
@@ -81,7 +81,7 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
     auto* deuteron_iso = new G4Isotope("D",
                                        IsotopeAtomicNumber{1},
                                        NumberOfNucleons{2},
-                                       MoleMass{2.01410178 * g / mole});
+                                       MoleMass{2.01410178 * g_per_mole});
     auto* deuteron_elem
         = new G4Element("Deuterium", "D", NumIsotopeComponents{1});
     deuteron_elem->AddIsotope(deuteron_iso, /* abundance fraction */ 1);
@@ -89,7 +89,7 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
     auto* triton_iso = new G4Isotope("T",
                                      IsotopeAtomicNumber{1},
                                      NumberOfNucleons{3},
-                                     MoleMass{3.0160492 * g / mole});
+                                     MoleMass{3.0160492 * g_per_mole});
     auto* triton_elem = new G4Element("Tritium", "T", NumIsotopeComponents{1});
     triton_elem->AddIsotope(triton_iso, /* abundance fraction */ 1);
 
@@ -111,11 +111,12 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
         hydrogen_fraction = 0;
     }
 
-    double hydrogen_density = hydrogen_fraction * 1.007825031898 * g / mole
+    double hydrogen_density = hydrogen_fraction * 1.007825031898 * g_per_mole
                               * liquid_hydrogen_density_ * target_.density;
-    double deuteron_density = target_.deuteron_fraction * 2.01410178 * g / mole
-                              * liquid_hydrogen_density_ * target_.density;
-    double triton_density = target_.triton_fraction * 3.0160492 * g / mole
+    double deuteron_density = target_.deuteron_fraction * 2.01410178
+                              * g_per_mole * liquid_hydrogen_density_
+                              * target_.density;
+    double triton_density = target_.triton_fraction * 3.0160492 * g_per_mole
                             * liquid_hydrogen_density_ * target_.density;
     double material_density = hydrogen_density + deuteron_density
                               + triton_density;
@@ -131,7 +132,7 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
                                 deuteron_density / material_density);
     target_material->AddElement(triton_elem, triton_density / material_density);
 
-    double env_size_xy = 40. * cm, env_size_z = 40. * cm;
+    double env_size_xy = 40. * CLHEP::cm, env_size_z = 40. * CLHEP::cm;
 
     // World volume definition
     G4double world_size_xy = 2.2 * env_size_xy;
@@ -148,7 +149,7 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
         nullptr, G4ThreeVector(), world_lv, "world_pv", 0, false, 0, true);
 
     // Target volume definition
-    G4double center_pos_x = 35.0 * cm;
+    G4double center_pos_x = 35.0 * CLHEP::cm;
 
     auto* target_solid = new G4Box("target_solid",
                                    0.1 * env_size_xy,
@@ -168,18 +169,19 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
     // Neutron detectors
     // todo: add the EPJ material soon. Only SD; no optical properties
     // Array of transverse dets (16 cm x 8 cm x 2 cm)
-    double detector_x = 16. * cm;
-    double detector_y = 8. * cm;
-    double detector_z = 2. * cm;
-    double detector_t_yz_pos = 5.0 * cm;
-    double rotation = 0. * deg;
+    double detector_x = 16 * CLHEP::cm;
+    double detector_y = 2 * CLHEP::cm;
+    double detector_z = 8 * CLHEP::cm;
 
     auto* scintillator_material
         = nist->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
     auto* detector_t_solid = new G4Box(
-        "det_t_solid", 0.5 * detector_x, 0.5 * detector_z, 0.5 * detector_y);
+        "det_t_solid", 0.5 * detector_x, 0.5 * detector_y, 0.5 * detector_z);
     auto* detector_t_lv = new G4LogicalVolume(
         detector_t_solid, scintillator_material, "det_t_lv");
+
+    double detector_t_yz_pos = 5.0 * CLHEP::cm;
+    double rotation = 0;
 
     for (int copy_num = 0; copy_num < 4; copy_num++)
     {
@@ -188,8 +190,8 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
         rotation_matrix->rotateX(rotation_angle);
         auto detector_pos = G4ThreeVector(
             center_pos_x,
-            detector_t_yz_pos * cos(rotation + (90.0 * deg) * copy_num),
-            detector_t_yz_pos * sin(rotation + (90.0 * deg) * copy_num));
+            detector_t_yz_pos * cos(rotation + (90.0 * CLHEP::deg) * copy_num),
+            detector_t_yz_pos * sin(rotation + (90.0 * CLHEP::deg) * copy_num));
 
         new G4PVPlacement(rotation_matrix,
                           detector_pos,
@@ -203,45 +205,49 @@ G4VPhysicalVolume* MucfTestGeo::build_geometry()
 
     // Back detector with 2 cm center hole (8 cm x 8 cm x 2 cm)
     auto* detector_back_solid_full = new G4Box("det_b_solid_full",
-                                               0.5 * detector_z,
                                                0.5 * detector_y,
-                                               0.5 * detector_y);
+                                               0.5 * detector_z,
+                                               0.5 * detector_z);
+    auto* hole = new G4Tubs("hole_solid",
+                            0 * CLHEP::cm,
+                            1 * CLHEP::cm,
+                            detector_y,
+                            0,
+                            360 * CLHEP::deg);
 
-    auto* hole = new G4Tubs(
-        "hole_solid", 0. * cm, 1.0 * cm, detector_z, 0. * deg, 360. * deg);
     auto* rotation_matrix_z = new G4RotationMatrix();
-    rotation_matrix_z->rotateY(90. * deg);
-    auto* detector_back_solid
-        = new G4SubtractionSolid("det_b_solid",
-                                 detector_back_solid_full,
-                                 hole,
-                                 rotation_matrix_z,
-                                 G4ThreeVector(0.0 * cm, 0.0 * cm, 0.0 * cm));
+    rotation_matrix_z->rotateY(90 * CLHEP::deg);
+
+    auto* detector_back_solid = new G4SubtractionSolid("det_b_solid",
+                                                       detector_back_solid_full,
+                                                       hole,
+                                                       rotation_matrix_z,
+                                                       G4ThreeVector());
 
     auto* detector_back_lv = new G4LogicalVolume(
         detector_back_solid, scintillator_material, "det_b_lv");
     new G4PVPlacement(nullptr,
-                      G4ThreeVector(26.0 * cm, 0., 0.),
+                      G4ThreeVector(26 * CLHEP::cm, 0, 0),
                       detector_back_lv,
                       "det_b_pv",
                       world_lv,
                       false,
-                      1,
+                      0,
                       true);
 
     // Forward detector (8 cm x 8 cm x 2 cm)
-    G4Box* solidDetF = new G4Box(
-        "det_f_solid", 0.5 * detector_z, 0.5 * detector_y, 0.5 * detector_y);
+    G4Box* detector_f_solid = new G4Box(
+        "det_f_solid", 0.5 * detector_y, 0.5 * detector_z, 0.5 * detector_z);
 
-    auto* detector_f_lv
-        = new G4LogicalVolume(solidDetF, scintillator_material, "det_f_lv");
+    auto* detector_f_lv = new G4LogicalVolume(
+        detector_f_solid, scintillator_material, "det_f_lv");
     new G4PVPlacement(nullptr,
-                      G4ThreeVector(44.0 * cm, 0.0 * cm, 0.0 * cm),
+                      G4ThreeVector(44 * CLHEP::cm, 0, 0),
                       detector_f_lv,
                       "det_f_pv",
                       world_lv,
                       false,
-                      1,
+                      0,
                       true);
 
     return world_pv;
