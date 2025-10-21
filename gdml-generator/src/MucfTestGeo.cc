f52e248542c34f69f3ddd5fa24150153558a66b4
Stefano Tognini <togninis@ornl.gov>
2025-10-21 14:59:34 -0400

    Fix includes

--- gdml-generator/src/MucfTestGeo.cc
+++ gdml-generator/src/MucfTestGeo.cc
@@ -10,17 +10,12 @@
 #include <G4LogicalVolume.hh>
 #include <G4NistManager.hh>
 #include <G4PVPlacement.hh>
+#include <G4RotationMatrix.hh>
 #include <G4SDManager.hh>
+#include <G4SubtractionSolid.hh>
+#include <G4Transform3D.hh>
+#include <G4Tubs.hh>
 
-#include "G4Box.hh"
-#include "G4Cons.hh"
-#include "G4Orb.hh"
-#include "G4RotationMatrix.hh"
-#include "G4Sphere.hh"
-#include "G4SubtractionSolid.hh"
-#include "G4Transform3D.hh"
-#include "G4Trd.hh"
-#include "G4Tubs.hh"
 #include "core/SensitiveDetector.hh"
 
 //---------------------------------------------------------------------------//
