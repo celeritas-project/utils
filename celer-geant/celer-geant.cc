40d044aa4ccfc59c87a8d14b85222f1d5b52ba38
Stefano Tognini <togninis@ornl.gov>
2026-06-22 11:30:11 -0400

    Fix celeritas version warning in celer-geant

--- celer-geant/celer-geant.cc
+++ celer-geant/celer-geant.cc
@@ -58,9 +58,7 @@ int main(int argc, char* argv[])
     // Initialize physics with Celeritas offload
     using PhysicsOptions = celeritas::GeantPhysicsOptions;
     using MuonPhysicsOptions = celeritas::GeantMuonPhysicsOptions;
-    auto phys_opts = PhysicsOptions::deactivated();
-    phys_opts.muon = MuonPhysicsOptions{};
-    phys_opts.muon.msc = celeritas::MscModelSelection::none;
+    auto phys_opts = PhysicsOptions{};
 
     auto physics = std::make_unique<celeritas::EmPhysicsList>(phys_opts);
     physics->RegisterPhysics(new celeritas::TrackingManagerConstructor(&tmi));
